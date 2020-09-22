bd57c7bf0f2d1d84e94d07fd2463230e3b586c71
Allie <williamson.alisonj@gmail.com>
2020-09-22 00:11:19 -0400

    created example names for burgers in seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Classic Cheeseburger', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Burger', TRUE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Toasty Bun Burger', TRUE);
